cc09744d45c423234e62727277bd32f95d490483
rodrigo <rodrigo.aze7@gmail.com>
2017-05-17 21:01:55 -0300

    Formset do carrinho feito

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.views.generic import RedirectView, TemplateView
 from .models import CartItem
 from catalogo.models import Product
 from django.contrib import messages
+from django.forms import modelformset_factory
+from django.core.urlresolvers import reverse_lazy
 
 class CreateCartItemView(RedirectView):
 
@@ -18,12 +20,49 @@ class CreateCartItemView(RedirectView):
 		else:
 			messages.success(self.request, 'Produto atualizado com sucesso')
 
-		return product.get_absolute_url()
+		return reverse_lazy('checkout:cart_item')
 
 
-class CartDetailItemView(TemplateView):
+#Classe de exibição do formulário do carrinho
+class CartItemView(TemplateView):
+
 	template_name = 'checkout/cart.html'
-	def get_context_data(self, **kwargs):
+
+	def get_form_set(self, clear=False):
+
+		CartItemFormSet = modelformset_factory(
+			CartItem, fields=('quantity',), can_delete=True, extra=0
+		)
+		session_key = self.request.session.session_key
+		#Se houver sessão, então há mudanças no cart
+		if session_key:
+			if clear:
+				formset = CartItemFormSet(
+					queryset=CartItem.objects.filter(cart_key=session_key)
+                )
+			else:
+			    formset = CartItemFormSet(
+                    queryset=CartItem.objects.filter(cart_key=session_key),
+                    data=self.request.POST or None
+                )
+		else:
+			formset = CartItemFormSet(queryset=CartItem.objects.none())
+		return formset
+
+	def get_context_data(self, **kwargs): 
 		context = super(CartItemView, self).get_context_data(**kwargs)
+		context['formset'] = self.get_form_set()
 		return context
+
+	def post(self, request, *args, **kwargs):
+		formset = self.get_form_set()
+		context = self.get_context_data(**kwargs)
+		if formset.is_valid():
+			formset.save()
+			messages.success(request, 'Carrinho atualizado com sucesso')
+			context['formset'] = self.get_form_set(clear=True)
+		return self.render_to_response(context)
+
+
 create_cartitem = CreateCartItemView.as_view()
+cart_item = CartItemView.as_view()
