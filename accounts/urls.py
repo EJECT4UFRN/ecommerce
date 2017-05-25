d6e85685b62ffc7c7b9d7c078e3cf2d7672f1de7
rodrigo <rodrigo.aze7@gmail.com>
2017-05-25 13:56:53 -0300

    Heroku, run

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^alterar-dados/$', views.update_user, name='update_user'),
 	url(r'^alterar-senha/$', views.update_password, name='update_password'),
-	url(r'^atuzalizar/$', views.register, name='register'),
+	url(r'^atualizar/$', views.register, name='register'),
 	#url(r'^meus-pedidos/$', include('checkout.urls', namespace='checkout'))
 
 ]
